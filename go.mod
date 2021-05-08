39c58569a3cb6405f9938ce4bffa96b09b45f493
lewinz <18685031562@163.com>
2021-05-08 15:47:13 +0800

    .

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module blog_project
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+
+	engine.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "the page 404",
+		})
+	})
 }
